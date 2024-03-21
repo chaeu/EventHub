c1cf4742cdbf77d96cee6a37ab30f569ec33b508
chaeu <christoph.haeusler@aon.at>
2024-03-21 16:19:59 +0100

    Add user registration process

--- base/views.py
+++ base/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Event, Type
 from .forms import EventForm
 
 
 def loginPage(request):
+    page = "login"
+    if request.user.is_authenticated:
+        return redirect("home")
+    
     if request.method == "POST":
-        username = request.POST.get("username")
+        username = request.POST.get("username").lower()
         password = request.POST.get("password")
 
         try:
@@ -25,12 +32,29 @@ def loginPage(request):
         else:
             messages.error(request, "Username Or Password was not correct")
 
-    context = {}
+    context = {"page":page}
     return render(request, "base/login_register.html", context)
 
 def logoutUser(request):
     logout(request)
     return redirect("home")
+
+def registerPage(request):    
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        print(request)
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "An error occurred during registration")
+
+    return render(request, "base/login_register.html", {"form":form})
 
 def home(request):
     q = request.GET.get("q") if request.GET.get("q") != None else ""
@@ -53,6 +77,7 @@ def event(request, pk):
     context = {"event":event}
     return render(request, "base/event.html", context)
 
+@login_required(login_url="login")
 def createEvent(request):
     form = EventForm
     if request.method == "POST":
@@ -63,9 +88,13 @@ def createEvent(request):
     context = {"form": form}
     return render(request, "base/event_form.html", context)
 
+@login_required(login_url="login")
 def updateEvent(request, pk):
     event = Event.objects.get(id=pk)
     form = EventForm(instance=event)
+
+    if request.user != event.host:
+        return HttpResponse("You are not the owner!")
 
     if request.method == "POST":
         form = EventForm(request.POST, instance=event)
@@ -75,6 +104,7 @@ def updateEvent(request, pk):
     context = {"form": form}
     return render(request, "base/event_form.html", context)
 
+@login_required(login_url="login")
 def deleteEvent(request, pk):
     event = Event.objects.get(id=pk)
     if request.method == "POST":
